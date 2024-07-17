103c7544bcccfcce2e7d0b944c7acca04fa5024f
RajAdhikar7 <adhikarraj77@gmail.com>
2024-07-17 15:04:33 +0530

    updated basic.py

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,8 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+#endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/" #http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
 print("this is text",get_response.text) # print raw text response
@@ -10,5 +11,7 @@ print("this is text",get_response.text) # print raw text response
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Nototion ~ Python Dict
-print("this is json",get_response.json())
-print("this is status code",get_response.status_code)
+
+# print("this is json",get_response.json())
+# print("this is status code",get_response.status_code) 
+
