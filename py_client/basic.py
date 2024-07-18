651d7eb3a798e609dd643bb67672c13ee849752e
RajAdhikar7 <adhikarraj77@gmail.com>
2024-07-18 12:14:15 +0530

    converted model instances to dictionary

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,17 +1,12 @@
 import requests
 
-# endpoint = "https://httpbin.org/status/200/"
-#endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/" #http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
-print("this is text",get_response.text) # print raw text response
+endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/
 
-
+get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP Request
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
-# JavaScript Object Nototion ~ Python Dict
 
-# print("this is json",get_response.json())
+print(get_response.json())
 # print("this is status code",get_response.status_code) 
 
