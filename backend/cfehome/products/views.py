012121bdc9b8705020294d2ec9501463c3e2941b
RajAdhikar7 <adhikarraj77@gmail.com>
2024-07-22 17:22:47 +0530

    added function based views for create , list , detail

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -1,7 +1,10 @@
 
 from rest_framework import generics
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -38,11 +41,29 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 product_detail_view = ProductDetailAPIView.as_view() 
 
-class ProductListAPIView(generics.ListAPIView):
-    '''
-    Not gonna use this method
-    '''
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+@api_view(['GET','POST'])
+def product_alt_view(request,pk=None,*args,**kwargs):
+    method = request.method 
 
-product_list_view = ProductListAPIView.as_view() 
+    if method == "GET":
+        if pk is not None:
+            #detail view 
+            obj = get_object_or_404(Product , pk=pk)
+            data = ProductSerializer(obj,many=False).data
+            return Response(data)
+        #list view
+        queryset = Product.objects.all() 
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    
+    if method == "POST":
+        ## create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
