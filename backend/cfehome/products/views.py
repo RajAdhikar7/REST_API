0914c618c1e4fe8ebb6d40623114fec2c090ad34
RajAdhikar7 <adhikarraj77@gmail.com>
2024-07-22 18:28:04 +0530

    added updateapiview and deleteapiview

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -34,12 +34,37 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk' (by default)
 
 product_detail_view = ProductDetailAPIView.as_view() 
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        # instance 
+        super().perform_destroy(instance)
+
+product_destroy_view = ProductDestroyAPIView.as_view()
 
 @api_view(['GET','POST'])
 def product_alt_view(request,pk=None,*args,**kwargs):
