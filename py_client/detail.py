ab239666cc77ae44470695f2cf4df5845d904c62
RajAdhikar7 <adhikarraj77@gmail.com>
2024-07-28 22:11:42 +0530

    updated detail.py

--- py_client/detail.py
+++ py_client/detail.py
@@ -1,6 +1,10 @@
 import requests
 
+headers = {'Authorization': 'Bearer 447ecf7c7440335d8d28f650b3ccdfab70f45094'}
 endpoint = "http://localhost:8000/api/products/1/" 
 
-get_response = requests.get(endpoint) 
+get_response = requests.get(endpoint,headers=headers) 
 print(get_response.json())
+
+
+
