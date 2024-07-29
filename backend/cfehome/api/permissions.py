544b56975aa702160c14e8292fcc2dd4b12c1ee0
RajAdhikar7 <adhikarraj77@gmail.com>
2024-07-29 22:33:48 +0530

    added viewsets and routers

--- backend/cfehome/api/permissions.py
+++ backend/cfehome/api/permissions.py
@@ -10,4 +10,4 @@ class IsStaffEditorPermission(permissions.DjangoModelPermissions):
         'PUT': ['%(app_label)s.change_%(model_name)s'],
         'PATCH': ['%(app_label)s.change_%(model_name)s'],
         'DELETE': ['%(app_label)s.delete_%(model_name)s'],
-    }
+    } 
